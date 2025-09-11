e83ea12d4bb459b9e54efaa32ab295e277ede3e9
nini1234984 <osepashvilinini77@gmail.com>
2025-09-11 12:23:06 -0700

    second commit

---
+++ level 30/cw30.py
@@ -0,0 +1,11 @@
+# 1) ზემოთ მოცემულ ფუნქციებზე(.upper(), .lower(), .capitalize(), .title(), .find()) გააკეთეთ თითო მაგალითი
+name="nini"
+print(name.upper())
+print(name.lower())
+
+name1="me var nini"
+print(name1.capitalize())
+print(name1.title())
+
+name3="ME, VAR, NINI"
+print(name3.find("N"))
