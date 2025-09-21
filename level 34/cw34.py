f043d3d2e58d63396d874b7c55af0ae81d2eb480
nini1234984 <osepashvilinini77@gmail.com>
2025-09-21 04:23:17 -0700

    second commit

---
+++ level 34/cw34.py
@@ -0,0 +1,14 @@
+# 1) შექმენით ფუნქცია სახელად greet, რომელიც დაბეჭდავს მისალმების ტექსტებს. "Hello World!" და "Hello {name}"
+# def greet():
+#     print("Hello World!")
+#     print("Hello NINI")
+# greet()
+
+def my_len(name):
+
+    
+    for i in name():
+        
+        print(my_len("NINI"))
+    #bodishitt mas matematikis mere vegar vazrovnebbb xolmeee
+    
